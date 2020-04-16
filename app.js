1cff16b07835e214f243ba43274cff130bdbcf5c
khoi148 <khoiminhphan@yahoo.com>
2020-04-16 17:22:35 +0700

    added listener function

--- app.js
+++ app.js
@@ -41,5 +41,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
+app.listen(process.env.PORT, () => {
+  console.log("server is running on port " + process.env.PORT);
+});
 module.exports = app;
