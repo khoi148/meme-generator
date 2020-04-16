4ae6c9fcf600833001715f4413ec42a9a59e975a
khoi148 <khoiminhphan@yahoo.com>
2020-04-16 18:12:22 +0700

    no duplicates check added

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,7 @@
 const upload = require("../utils/upload.js");
 var express = require("express");
+var jimp = require("jimp");
+
 var router = express.Router();
 const {
   loadData,
@@ -17,8 +19,21 @@ router.post("/", upload, async (req, res) => {
       //if user didn't upload anything
       throw new Error("You need to select a file");
     }
+    let found = loadData().findIndex(
+      (item) => item.originalname === file.originalname
+    );
+    if (found !== -1) {
+      throw new Error("This file has already been uploaded. Choose another.");
+    }
+
+    //Use Jimp
+    // let image = await jimp(file.path);
+    // image.resize(300, Jimp.AUTO, Jimp.RESIZE_NEAREST_NEIGHBOR);
+    // image.write(file.path);
+
     // save Image information to database.
     addImage(file);
+
     return res.redirect("/browse");
   } catch (e) {
     return res.render("index", {
